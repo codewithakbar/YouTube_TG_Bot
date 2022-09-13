47f840d60a61bd9e6d58cafe4a6038cc70208306
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2022-09-13 17:12:18 +0500

    Add comment

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-TOKEN="5614781597:AAHlyHEGfk2vAcf0pRWmlfpgdsEYgDX8uh0"
+
+# Botni tokenini almashtirish esingdan chiqmasin,
+# bilaman productionga tokenni chiqarib qoyibman!
+TOKEN="5614781597:AAHlyHEGfk2vAcf0pRWmlfpgdsEYgDX8uh0" 
